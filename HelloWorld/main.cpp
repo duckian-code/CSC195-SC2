435678472042b52118cfaa372091364caaffd231
Lily <66748489+duckian-code@users.noreply.github.com>
2025-05-06 14:10:54 -0600

    Class 1

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    std::cout << "Hello World! \n";
+    std::cout << "Multiple Lines! \n";
+    cout << "Hello World! \n";
+    cout << "Multiple Lines! \n";
+
+    int i = 0;
+    cout << "Enter number: " << endl;
+    cin >> i;
+
+    return 0;
+}
